03ded626bfaefa3116f2d90b27f696acb8156d59
N3wSk1Y <miner.fedenko@gmail.com>
2022-05-08 12:37:39 +0300

    Files structure

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-const { app, BrowserWindow } = require('electron')
-
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 1920,
-      height: 1080
-    })
-  
-    win.loadFile('index.html')
-    win.setTitle("ZeroCoder")
-    win.center()
-}
-
-app.whenReady().then(() => {
-    createWindow()
-})  
+require("./src/windows/projects/projects.js");
