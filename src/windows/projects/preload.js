dd6603561d004598836bd3481f902a7875a55107
N3wSk1Y <miner.fedenko@gmail.com>
2022-05-15 15:21:42 +0300

    Added projects validation

--- src/windows/projects/preload.js
+++ src/windows/projects/preload.js
@@ -1,5 +1,25 @@
 const { Titlebar, Menu } = require("custom-electron-titlebar")
+const fs = require('fs')
+const p = require('path')
 
+const dir = 'C:\\Users\\Дмитрий\\ZeroCoderProjects' // TOEDIT
+
+function getProjects(dir, projects) {
+    projects = projects || [];
+    var allFiles = fs.readdirSync(dir);
+    for (var i = 0; i < allFiles.length; i++){
+        var path = dir + '/' + allFiles[i];
+        if (fs.statSync(path).isDirectory()){
+            getProjects (path, projects);
+        } else {
+            projects.push({
+                name: p.basename(path).slice(0, p.basename(path).indexOf(".")),
+                path: path
+            });
+        }
+    }
+    return projects;
+}
 
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
@@ -9,6 +29,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const { version } = require('../../../package.json')
     document.getElementById('version').innerHTML = version
+
+    console.log(getProjects(dir))
 
     // const titlebar = new Titlebar({
     //     maximizable: false,
