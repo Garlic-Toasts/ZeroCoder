741785f234149f158185642aa405067a298d1938
Gregory-coder <87940322+Gregory-coder@users.noreply.github.com>
2022-05-15 13:41:24 +0300

    Add files via upload

---
+++ src/windows/projects/front.js
@@ -0,0 +1,23 @@
+function newSolution() {
+	//...
+}
+function search() {
+	//...
+}
+function openButton() {
+	//...
+}
+function getFromVcs() {
+	//...
+}
+function projects() {
+	//...
+}
+function tutorials() {
+	//...
+}
+// function getVersion() {
+// 	var p = require("./././package.json") //Что-то такое, наверное
+// 	var vers = p["version"]
+// }
+// getVersion();
