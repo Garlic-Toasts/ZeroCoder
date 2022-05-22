2a793f99c2adc38b8489f323fe351f17347d3c95
N3wSk1Y <miner.fedenko@gmail.com>
2022-05-22 12:52:47 +0300

    `styles` refactoring

--- preload.js
+++ preload.js
@@ -4,24 +4,24 @@ const fs = require('fs')
 const p = require('path')
 const ipc = require('electron').ipcRenderer
 
-const dir = 'C:\\Users\\people\\Desktop\\Grisha\\School\\zerocoder\\test_projects' // TOEDIT
+const dir = 'C:\\Users\\Дмитрий\\ZeroCoderProjects' // TOEDIT
 
 const colors_1 = ["#14877F", "#772982", "#4BAF3B", "#ECA345", "#D5784D"]
 const colors_2 = ["#075852", "#31216D", "#226318", "#E47E3C", "#D64E3E"]
 const colors_3 = ["#CECECE", "#CECECE", "#CECECE", "#000000", "#000000"]
 
-uppers = function(pName) {
+const uppers = function (pName) {
     if (pName.includes(' ')) {
         let parts = pName.split(' ')
-        return parts[0][0].toUpperCase()+parts[1][0].toUpperCase()
+        return parts[0][0].toUpperCase() + parts[1][0].toUpperCase()
     } else if (pName.includes('-')) {
         let parts = pName.split('-')
-        return parts[0][0].toUpperCase()+parts[1][0].toUpperCase()
+        return parts[0][0].toUpperCase() + parts[1][0].toUpperCase()
     } else if (pName.includes('_')) {
         let parts = pName.split('_')
-        return parts[0][0].toUpperCase()+parts[1][0].toUpperCase()
+        return parts[0][0].toUpperCase() + parts[1][0].toUpperCase()
     } else if (/[A-Z]/g.test(pName)) {
-        return pName.match(/[A-Z]/g).slice(0,2).join("")
+        return pName.match(/[A-Z]/g).slice(0, 2).join("")
     } else if (/[0-9]/g.test(pName)) {
         return pName[0].toUpperCase() + pName.match(/[0-9]/g)[0]
     } else {
@@ -41,27 +41,27 @@ window.addEventListener('DOMContentLoaded', () => {
     openButton.addEventListener('click', function (event) {
         ipc.send('select-project-dialog')
     })
-    
-    if (fs.existsSync(dir)){
+
+    if (fs.existsSync(dir)) {
         var projectsList = getProjects(dir)
     } else {
         var projectsList = []
     }
     var list = document.getElementById('list')
-	for (var i = 0; i < projectsList.length; i++) {
-		let element = document.createElement('li');
-		element.innerText = projectsList[i].name
-		t = '<input type="radio" value="'+projectsList[i].path+'id="'+i+'_radio"name="radio-list"onClick="openProject()"'
-		if (i === 0) {
-			t += "hover"
-		}
-		t += '><label for="'+i+'_radio"><p class="p-name">'+projectsList[i].name+'</p><p class="p-path">'+projectsList[i].path+'</p></label>'
-		let r = Math.floor(Math.random()*5)
-		t += '<div class="minicon" style="background: linear-gradient(120deg,'+colors_1[r]+','+colors_2[r]+');"><p style="color:'+colors_3[r]+'">'
-        +uppers(projectsList[i].name)+'</p></div>'
-		element.innerHTML = t
-		element.className = "project"
-		list.appendChild(element)
+    for (var i = 0; i < projectsList.length; i++) {
+        let element = document.createElement('li');
+        element.innerText = projectsList[i].name
+        t = '<input type="radio" value="' + projectsList[i].path + 'id="' + i + '_radio"name="radio-list"onClick="openProject()"'
+        if (i === 0) {
+            t += "hover"
+        }
+        t += '><label for="' + i + '_radio"><p class="p-name">' + projectsList[i].name + '</p><p class="p-path">' + projectsList[i].path + '</p></label>'
+        let r = Math.floor(Math.random() * 5)
+        t += '<div class="minicon" style="background: linear-gradient(120deg,' + colors_1[r] + ',' + colors_2[r] + ');"><p style="color:' + colors_3[r] + '">'
+            + uppers(projectsList[i].name) + '</p></div>'
+        element.innerHTML = t
+        element.className = "project"
+        list.appendChild(element)
     }
 
     const titlebar = new Titlebar({
@@ -78,17 +78,17 @@ window.addEventListener('DOMContentLoaded', () => {
 function getProjects(dir, projects) {
     projects = projects || [];
     var allFiles = fs.readdirSync(dir);
-    for (var i = 0; i < allFiles.length; i++){
+    for (var i = 0; i < allFiles.length; i++) {
         var path = dir + '\\' + allFiles[i];
-        if (fs.statSync(path).isDirectory()){
-            getProjects (path, projects);
+        if (fs.statSync(path).isDirectory()) {
+            getProjects(path, projects);
         } else {
             if (p.extname(path) == ".bprint") {
                 projects.push({
                     name: p.basename(path).slice(0, p.basename(path).indexOf(".")),
                     path: path
                 });
-            }    
+            }
         }
     }
     return projects;
