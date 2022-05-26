d66dee87c420ab50ea6a9f22c3181c6c05f65ff7
N3wSk1Y <miner.fedenko@gmail.com>
2022-05-26 19:26:10 +0300

    Scriptshandler basement

---
+++ src/scriptshandler/back.py
@@ -0,0 +1,4 @@
+while True:
+    print(1)
+
+# Test py-file
---
+++ src/scriptshandler/compiler.ts
@@ -0,0 +1,15 @@
+import fs from 'fs';	
+import { exec } from 'child_process'
+const backscript: string = "./back.py";
+
+const fileContent = fs.readFileSync(backscript, "utf8");
+
+class ScriptsHandler {
+    public static runPy(path: string): void {
+        exec(`python ${path}`, function (error, stdOut, stdErr) {
+            console.log(stdOut);
+        });
+    }
+}
+
+
